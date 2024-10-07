142a4124223150344576d93ba3122f447871061a
riyaguptacb <riya.gupta@crowdbotics.com>
2024-10-07 09:45:59 +0530

    first commit

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,5 @@
+export default function SignupPage(){
+    return(
+        <div>signup page</div>
+    )
+}
---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+    try{
+
+    }
+    catch(error){
+        console.error("Something goes wrong while connecting moongoose");
+        console.error(error);
+    }
+    
+}
